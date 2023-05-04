4fcba01f877c303887bfc7b9a8826c21b7431fbf
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-05-04 09:17:58 -0300

    Commit 2.0 do ZÉ PAYSANDU!!!!!!!!!!!!!

--- CalculadoraSimples.java
+++ CalculadoraSimples.java
@@ -57,7 +57,6 @@ public class CalculadoraSimples {
             //Saída
             System.out.println(n1 + " - " + n2 + " = " + total);
 
-                System.out.println("\n\t\t\t -- Subtração --\n");
 
 
             } else if(op == 3){
